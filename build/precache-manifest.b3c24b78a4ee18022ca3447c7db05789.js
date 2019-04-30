b46b9c7f7663a5db8e8a50c5f55c35a17899d24d
Nikhil Kedia <nikhilkedia15@gmail.com>
2019-04-30 11:08:41 -0400

    commit

---
+++ build/precache-manifest.b3c24b78a4ee18022ca3447c7db05789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ead42c5e6df6ae33c4d288dfba653d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0579d7725a48164be0fa",
+    "url": "/static/css/main.9beda04f.chunk.css"
+  },
+  {
+    "revision": "568ba63ee9a3e1198c8a",
+    "url": "/static/js/2.df0a131b.chunk.js"
+  },
+  {
+    "revision": "0579d7725a48164be0fa",
+    "url": "/static/js/main.0b1686cd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a4970f77ff44aa7b8b9fffd0ee456421",
+    "url": "/static/media/blacksquare.a4970f77.svg"
+  }
+]);
